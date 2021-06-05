73ca58a15ac7a680898425ca433c4716ff19257e
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-05 16:50:28 +0200

    init project #10

---
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramsearch/NistagramSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NistagramSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
