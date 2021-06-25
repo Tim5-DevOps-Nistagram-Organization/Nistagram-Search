beb3819f4d2f1409e1971a462242763a465719e4
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-25 12:35:02 +0200

    Set up auth #16

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramsearch/config/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramsearch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import rs.ac.uns.ftn.devops.tim5.nistagramsearch.security.AuthenticationTokenFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        config.addAllowedHeader("authorization");
+        config.addAllowedHeader("content-type");
+        config.addAllowedHeader("XMLHttpRequest");
+
+        config.setMaxAge(3600L);
+
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilterBean()
+            throws Exception {
+        AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter();
+        authenticationTokenFilter
+                .setAuthenticationManager(authenticationManagerBean());
+        return authenticationTokenFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/init").permitAll()
+                .anyRequest().authenticated().and()
+                .headers().xssProtection();
+        // Custom JWT based authentication
+        httpSecurity.addFilterBefore(authenticationTokenFilterBean(),
+                UsernamePasswordAuthenticationFilter.class);
+        // CORS
+        httpSecurity.addFilterBefore(corsFilter(), ChannelProcessingFilter.class);
+    }
+}
---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramsearch/model/kafka/UserSecurity.java
@@ -0,0 +1,17 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramsearch.model.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSecurity {
+
+    private String username;
+    private String role;
+
+}
