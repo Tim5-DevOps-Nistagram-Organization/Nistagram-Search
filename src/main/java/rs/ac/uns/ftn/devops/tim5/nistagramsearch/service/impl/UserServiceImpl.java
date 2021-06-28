2c27e4e813861562bebea41542ed498082af3976
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-28 13:50:03 +0200

    Revert "Revert "Set up profile privacy#25""
    This reverts commit 58709ab2ddd2ae5962723eb8642e9e74504f058f.

--- src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramsearch/service/impl/UserServiceImpl.java
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramsearch/service/impl/UserServiceImpl.java
@@ -28,4 +28,11 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException("User"));
     }
 
+    @Override
+    public void setSettings(String username, boolean isPrivate) throws ResourceNotFoundException {
+        User user = findByUsername(username);
+        user.setIsPrivate(isPrivate);
+        userRepository.save(user);
+    }
+
 }
